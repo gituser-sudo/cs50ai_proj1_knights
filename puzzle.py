327fa4666758dacd37116db8c5327578acb00a70
gituser-sudo <48635872+gituser-sudo@users.noreply.github.com>
2026-03-11 21:51:37 +0000

    Wed, Mar 11, 2026, 4:51 PM -05:00

--- puzzle.py
+++ puzzle.py
@@ -9,6 +9,12 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+
+#   Model                        Knowledge
+AKnight  AKnave  BKnight  BKnave
+
+
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
