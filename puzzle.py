9bd5546dfec5ee6afb5e309a38ddf3e73a570421
gituser-sudo <48635872+gituser-sudo@users.noreply.github.com>
2026-03-14 03:43:10 +0000

    Fri, Mar 13, 2026, 10:43 PM -05:00

--- puzzle.py
+++ puzzle.py
@@ -62,7 +62,8 @@ knowledge1 = And(
     Implication(BKnight, YouareKnight),
     Or(
         # Knave should have at least one untruth
-        And(AKnave, Or(Not(IamKnave), Not(YouareKnave))),
+        And(Or(AKnave, BKnave), Or(Not(IamKnave), Not(YouareKnave))),
+        And(Or(AKnave, BKnight), Or(Not(IamKnave), Not(YouareKnave))),
         # Knight should be all true
         And(AKnight, And(IamKnave, YouareKnave)),
 
